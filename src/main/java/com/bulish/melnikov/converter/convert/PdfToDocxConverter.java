ccb02fa76a9267151e14a1039732d3098520d4dd
Evgesha-thunder <72350114+Evgesha-thunder@users.noreply.github.com>
2025-03-20 18:29:05 +0300

    Convert pdf.
    * task delete old request
    
    * fix: fix error handling.
    
    * fix: fix error handling 2.
    
    * convert pdf to docx.

--- src/main/java/com/bulish/melnikov/converter/convert/PdfToDocxConverter.java
+++ src/main/java/com/bulish/melnikov/converter/convert/PdfToDocxConverter.java
@@ -1,8 +1,14 @@
 package com.bulish.melnikov.converter.convert;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 
 @Component
 public class PdfToDocxConverter extends PdfConverter {
@@ -12,6 +18,25 @@ public class PdfToDocxConverter extends PdfConverter {
 
     @Override
     public byte[] convert(File file) {
-        return null;
+        byte [] converterFile = null;
+
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+             PDDocument document = PDDocument.load(file);
+             XWPFDocument docx = new XWPFDocument()) {
+
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String text = pdfStripper.getText(document);
+
+            XWPFParagraph paragraph = docx.createParagraph();
+            paragraph.createRun().setText(text);
+
+            docx.write(out);
+            converterFile = out.toByteArray();
+
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return converterFile;
     }
 }
